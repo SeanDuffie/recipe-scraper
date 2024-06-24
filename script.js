b142fc34fdc966be15df0e1d600f94f59a6e9499
SeanDuffie <duffiesean@gmail.com>
2024-06-24 18:31:37 -0400

    added example to look for in first URL

---
+++ script.js
@@ -0,0 +1,30 @@
+<script data-cfasync="false" data-no-optimize="1">(
+        function () {
+            var clsElements = document.querySelectorAll("script[id^='cls-']");
+            window.adthriveCLS && clsElements && clsElements.length === 0 ? window.adthriveCLS.injectedFromPlugin = false : "";
+        }
+    )();
+</script><script type="text/javascript">(function (d) {var f = d.getElementsByTagName('SCRIPT')[0],p = d.createElement('SCRIPT');
+p.type = 'text/javascript';
+p.async = true;
+p.src = '//assets.pinterest.com/js/pinit.js';
+f.parentNode.insertBefore(p, f);
+})(document);
+</script>
+<script type="text/javascript">
+var wprmpuc_recipe_55476 = {
+    "ingredients": [
+        {"amount":"3","unit":"","name":"very ripe bananas","notes":"(medium\/large)","converted":{"2":{"amount":"3","unit":""}},"id":13188},
+        {"amount":"\u00bd","unit":"cup","name":"unsalted butter","notes":"(8 Tbsp) at room temperature","converted":{"2":{"amount":"113.5","unit":"g"}},"id":9630},
+        {"amount":"3\/4","unit":"cup","name":"granulated sugar","notes":"","converted":{"2":{"amount":"150","unit":"g"}},"id":9636},
+        {"amount":"2","unit":"","name":"large eggs","notes":"lightly beaten","converted":{"2":{"amount":"2","unit":""}},"id":9611},
+        {"amount":"1 \u00bd","unit":"cups","name":"all-purpose flour","notes":"","converted":{"2":{"amount":"187.5","unit":"g"}},"id":9624},
+        {"amount":"1","unit":"tsp","name":"baking soda","notes":"","converted":{"2":{"amount":"1","unit":"tsp"}},"id":9854},
+        {"amount":"\u00bd","unit":"tsp","name":"salt","notes":"","converted":{"2":{"amount":"\u00bd","unit":"tsp"}},"id":9618},
+        {"amount":"\u00bd","unit":"tsp","name":"vanilla extract","notes":"","converted":{"2":{"amount":"\u00bd","unit":"tsp"}},"id":9635},
+        {"amount":"1","unit":"cup","name":"walnuts","notes":"","converted":{"2":{"amount":"117","unit":"g"}},"id":10453},
+        {"amount":"\u00bd","unit":"cup","name":"raisins","notes":"","converted":{"2":{"amount":"72.5","unit":"g"}},"id":9900}]};
+</script>
+<script>window.wprm_recipes = {"recipe-55476":{"type":"food","name":"Moist Banana Bread Recipe","slug":"wprm-moist-banana-bread-recipe","servings":"10","rating":{"count":5380,"total":26786,"average":4.98,"user":0}}}</script><script>window.addEventListener("load",function(){var t=document.querySelectorAll("script[data-delay]");
+[].forEach.call(t,function(t){setTimeout(function(){t.src=t.dataset.src,t.removeAttribute("data-src")},parseInt(t.dataset.delay,10))})});
+</script>
