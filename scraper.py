cd7bbc727eb8050276bb6a11bcabdd10dba0074c
SeanDuffie <duffiesean@gmail.com>
2024-06-25 11:02:35 -0400

    Added bypass for websites that try to block scrapers Added a variable for the find_all tags Added ability to parse class attribute values

--- scraper.py
+++ scraper.py
@@ -21,7 +21,10 @@ def scrape_website(url: str) -> BeautifulSoup:
         BeautifulSoup: A parsed and objectified version of the html document from the link.
     """
     # Perform the request
-    response = requests.get(url=url, timeout=10)
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
+    }
+    response = requests.get(url=url, headers=headers, timeout=10)
 
     # Check if the HTTP request was successful
     assert response.status_code == 200
@@ -57,7 +60,7 @@ def parse_html(soup: BeautifulSoup) -> pd.DataFrame:
     options = pd.DataFrame(option_list)
     return options[0]
 
-def parse_html2(soup: BeautifulSoup) -> pd.DataFrame:
+def parse_html2(soup: BeautifulSoup, tag: str) -> pd.DataFrame:
     """ Extracts useful information from the parsed HTML.
 
     Args:
@@ -67,7 +70,7 @@ def parse_html2(soup: BeautifulSoup) -> pd.DataFrame:
         pd.DataFrame: DataFrame that was constructed and formatted from html data
     """
     ingredients = pd.DataFrame(columns=['Amount', 'Unit', 'Name'])
-    spans: ResultSet = soup.find_all("span")
+    spans: ResultSet = soup.find_all(tag)
 
     quant: List[float] = []
     unit: List[str] = []
@@ -80,8 +83,23 @@ def parse_html2(soup: BeautifulSoup) -> pd.DataFrame:
         # for i, key in enumerate(['data-ingredient-quantity', 'data-ingredient-unit', 'data-ingredient-name']):
         if len(elem.attrs) > 0:
             for res in elem.attrs.keys():
+                if res == 'class':
+                    clss = elem.attrs.get(res)
+
+                    if 'amount' in clss[0]:
+                        quant.append(elem.text)
+                        row[0] = elem.text
+                    elif 'unit' in clss[0]:
+                        unit.append(elem.text)
+                        row[1] = elem.text
+                    elif 'name' in clss[0]:
+                        name.append(elem.text)
+                        row[2] = elem.text
+                        ingredients.loc[c] = row
+                        row = ['', '', '']
+                        c += 1
                 if 'ingredient' in res:
-                    if 'amount' in res or 'quantity' in res:
+                    if 'quantity' in res:
                         quant.append(elem.text)
                         row[0] = elem.text
                     elif 'unit' in res:
@@ -113,11 +131,22 @@ def parse_table(soup: BeautifulSoup) -> pd.DataFrame:
 
 
 if __name__ == "__main__":
-    URL = "https://natashaskitchen.com/banana-bread-recipe-video/"
+    URL1 = "https://natashaskitchen.com/banana-bread-recipe-video/"
     URL2 = "https://www.allrecipes.com/recipe/283621/the-perfect-deviled-eggs/"
     URL3 = "https://www.lilluna.com/creamy-swiss-chicken-bake/"
-    HTML = scrape_website(url=URL3)
-    # db = parse_table(soup=HTML)
-    db = parse_html2(soup=HTML)
-    # db.to_csv(path_or_buf=f"{RTDIR}/groceries.csv", index=False, header=False)
+
+    HTML1 = scrape_website(url=URL1)
+    db1 = parse_html2(soup=HTML1, tag=['script'])
+    # db1.to_csv(path_or_buf=f"{RTDIR}/groceries.csv", index=False, header=False)
+    # db1 = parse_table(soup=HTML)
+    print(db1)
+
+    HTML2 = scrape_website(url=URL2)
+    db2 = parse_html2(soup=HTML2, tag=['span'])
+    # db2.to_csv(path_or_buf=f"{RTDIR}/groceries.csv", index=False, header=False)
+    print(db2)
+
+    HTML3 = scrape_website(url=URL3)
+    db = parse_html2(soup=HTML3, tag=['span', 'p'])
+    # db2.to_csv(path_or_buf=f"{RTDIR}/groceries.csv", index=False, header=False)
     print(db)
